7c20d6afe55116aa3afbb357b2acc6ebfdf67cdd
Frank Guerhardt De Freitas <111311043+Frank-Freitas@users.noreply.github.com>
2025-02-15 01:58:57 -0300

    modificado configurações do banco de dados

--- restaurante/rest/src/main/java/com/restaurante/rest/DbConfig.java
+++ restaurante/rest/src/main/java/com/restaurante/rest/DbConfig.java
@@ -8,8 +8,8 @@ public class DbConfig {
 
   private static final String URL =
     "jdbc:postgresql://localhost:5432/RestauranteDB";
-  private static final String USER = "postgres";
-  private static final String PASSWORD = "9guerhardt9";
+  private static final String USER = //adicionar seu usuario do banco de dados ;
+  private static final String PASSWORD = //adicionar a senha do seu banco dedados;
 
   public static Connection getConnection() throws SQLException {
     return DriverManager.getConnection(URL, USER, PASSWORD);
